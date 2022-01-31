33015cdb417e447f7dc06c840ba40d8ba3c3ec34
a.lisa <andreilisa2001@gmail.com>
2022-01-31 17:26:35 +0200

    implement all crud methods for this project

--- cat-app/src/app/app-routing.module.ts
+++ cat-app/src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/type' },
-  { path: 'type', loadChildren: () => import('./components/type-list/type-list.module').then(m => m.TypeListModule) },
-  { path: 'create', loadChildren: () => import ("./components/add-type/add-type.module").then(m => m.AddTypeModule) },
-  { path: 'delete-update', loadChildren: () => import ("./components/delete-update/delete-update.module").then(m => m.DeleteUpdateModule) }
+  { path: 'type', loadChildren: () => import('./components/type-list/type-list.module').then(m => m.TypeListModule) }
 ];
 
 @NgModule({
